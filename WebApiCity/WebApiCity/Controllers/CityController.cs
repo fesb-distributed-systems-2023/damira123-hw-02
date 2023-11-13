a30c2be2fea41220fe41dd06aa266a4babaef808
Damira123 <148005498+Damira123@users.noreply.github.com>
2023-11-13 22:20:35 +0100

    ispravak grešaka

--- WebApiCity/WebApiCity/Controllers/CityController.cs
+++ WebApiCity/WebApiCity/Controllers/CityController.cs
@@ -10,7 +10,7 @@
 *          "Resident {Resident name} lives in {City name} City :: from route"
 *      2. Takes two parameters (Resident name, City name) from Query and return response with status OK and text
 *          "Resident {Resident name} lives in {City name} City :: from query"
-*      3. Takes one parameter (Airport name) from Body and return response with status OK and textž
+*      3. Takes one parameter (City name) from Body and return response with status OK and tekst
 *          "Hello from {City name} City!"
 *
 **********************************
@@ -26,14 +26,14 @@ namespace WebApiCity.Controllers
     {
         [HttpGet]
         [Route("/cities/{residentName}/{cityName}")]
-        public IActionResult RouteGet([FromRoute] string residentName, string cityName)
+        public IActionResult RouteGet([FromRoute] string residentName, [FromRoute] string cityName)
         {
             return Ok($"Resident {residentName} lives in {cityName} City :: from route");
         }
 
         [HttpGet]
         [Route("/query")]
-        public IActionResult QueryGet([FromQuery] string residentName, string cityName)
+        public IActionResult QueryGet([FromQuery] string residentName, [FromQuery] string cityName)
         {
             return Ok($"Resident {residentName} lives in {cityName} City :: from query");
         }
